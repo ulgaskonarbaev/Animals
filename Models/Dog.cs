70fb242d3e059a915baa98a122154b3c24b27c23
Ulgas Konarbaeyev <ulgas.konarbaev@bk.ru>
2023-04-02 12:15:33 +0600

    make commit of fils

---
+++ Models/Dog.cs
@@ -0,0 +1,12 @@
+﻿namespace Animals.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public int Weight { get; set; }
+
+
+    }
+}
